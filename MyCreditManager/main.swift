dfcdf7495e6c0e1f202b4d961ca5216f0842054c
cestbonciel <cestbonciel88@knou.ac.kr>
2022-11-22 04:20:06 +0900

    2nd commit to gihub for trying to make codes..

--- MyCreditManager/main.swift
+++ MyCreditManager/main.swift
@@ -6,59 +6,36 @@
 //
 
 import Foundation
-//let condition0 = """
-//원하는 기능을 입력해주세요
-//1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료
-//"""
-//print(condition0)
-
-//print("Hello, World!")
-//if let input = readLine() {
-//	print("input:\(input)")
-//}
-
-
-//func gradeManage(select: Int)
-//{
-//	let condition1 = """
-//	원하는 기능을 입력해주세요
-//	1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료
-//	"""
-//	print(condition1)
-//var select = Int(readLine()!)!
 let condition1 = """
 원하는 기능을 입력해주세요
 1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료
 """
 print(condition1)
-var select = readLine()!
-while select != "X"{
 
+let exit = "X"
+
+//let str = "X"
+var studentName: [String] = []
+let selectOne = Int(readLine()!)!
+// optional 에 다운캐스팅 해야하나?
+if selectOne != nil {
+//	(Int((readLine() ?? nil)!)! != nil) != nil
+//	while input != str {
+//		print("input: \(input)")
+//	let selectOne = Int(readLine()!)!
+		let name = readLine()!
+		if selectOne == 1  {
+			print("추가할 학생의 이름을 입력해주세요.")
+			if studentName.contains(name) == false  {
+				studentName.append(name)
+				print("\(name) 학생을 추가했습니다.")
+			}
+		} else {
 	
-	var chooseOne = Int(readLine()!)!
-	switch chooseOne
-	{
-	case 1:
-		print("추가할 학생의 이름을 입력해주세요")
-		print(condition1)
-	case 2:
-		print("삭제할 학생의 이름을 입력해주세요")
-		print(condition1)
-	case 3:
-		print("성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A, F 등)을 띄어쓰기로 구분하여 차례로 작성해주세요.")
-		print(condition1)
-	case 4:
-		print("성적을 삭제할 학생의 이름, 과목 이름을 띄어쓰기로 구분하여 차례로 작성해주세요.")
-		print(condition1)
-	case 5:
-		print("평점을 알고싶은 학생의 이름을 입력해주세요")
-	default: print("뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X 를 입력해주세요.")
-		print(condition1)
-	}
+			print("\(name)은 이미 존재하는 학생입니다. 추가하지 않습니다.")
+		}
+//	}
+} else {
+	print("입력이 잘못되었습니다. 다시 확인해주세요.")
 }
-
-
-//}
-
-
-
+print("프로그램을 종료한다.")
